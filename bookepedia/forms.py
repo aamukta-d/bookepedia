ce5da786a1c4eefc6273b361fbf6a51d8a834dba
Mateusz Kloska <2678207K@student.gla.ac.uk>
2024-03-08 22:19:30 +0000

    Book genre fixed?

--- bookepedia/forms.py
+++ bookepedia/forms.py
@@ -1,11 +1,11 @@
 from django import forms 
-from bookepedia.models import Book
+from .models import Book
 
 class BookForm(forms.ModelForm):
-    title = forms.CharField(max_length = 128, help_text = "Please enter the title of the book.")
-    author = forms.CharField(max_length = 128, help_text = "Please enter the author's name.")
-    description = forms.CharField(max_length = 128, help_text = "Please provide a short description.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the book.")
+    author = forms.CharField(max_length=128, help_text="Please enter the author's name.")
+    description = forms.CharField(max_length=128, help_text="Please provide a short description.")
 
     class Meta:
         model = Book
-        fields = ('title','author','description','genre')
+        fields = ('title', 'author', 'description', 'genre')
--- bookepedia/models.py
+++ bookepedia/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-# Create your models here.
-
 class Book(models.Model):
-    GENRE_CHOICES = [
-        ('Fantasy', 'Fantasy'),
-        ('Sci_Fi', 'Sci-Fi')
-    ]
     title = models.CharField(max_length = 128, blank = True)
     author = models.CharField(max_length = 128, blank = True)
     description = models.CharField(max_length = 128, blank = True)
-    genre = models.CharField(max_length=128, choices=GENRE_CHOICES, help_text = "Please select a genre.")
-
+    GENRE_CHOICES = [
+        ('Fantasy', 'Fantasy'),
+        ('Sci-Fi', 'Sci-Fi'),
+    ]
+    genre = models.CharField(max_length=128, choices=GENRE_CHOICES, help_text="Please select a genre.")
     def __str__(self): 
         return self.title
